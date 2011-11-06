20f09003f7af91ea669d88dbd2976200bd43c31b
Cash Costello <cash.costello@gmail.com>
2011-11-06 13:45:30 -0500

    1.8 version of referrer check

---
+++ languages/en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Language string for simple referer check plugin
+ */
+
+$english = array(
+	'src:error' => 'Invalid referrer for this action. Please contact %s for more information.',
+);
+
+add_translation('en', $english);
--- start.php
+++ start.php
@@ -1,38 +1,67 @@
-<?
+<?php
 /**
  * Hooks into action, all and checks it against a list of valid referrers.
  */
 
-function src_check($hook, $action, $return, $params) {
-	$url = get_config('url');
+elgg_register_plugin_hook_handler('action', 'all', 'src_check');
+
+/**
+ * Check the referer
+ *
+ * @param string $hook   Hook name
+ * @param sting  $action Action name
+ * @param bool   $return Whether this action will be processed
+ * @return void
+ */
+function src_check($hook, $action, $return) {
+	$url = elgg_get_site_url();
+
 	// action => valid referrer (can be array)
 	$actions_referers = array(
-		'register' => array($url . 'pg/register/', $url . 'account/register.php')
+		'register' => array($url . 'register'),
 	);
 
 	if (array_key_exists($action, $actions_referers)) {
 		src_log();
+
 		$valid = $actions_referers[$action];
-		$current = $_SERVER['HTTP_REFERER'];
 		if (!is_array($valid)) {
 			$valid = array($valid);
 		}
 
-		foreach ($valid as $referer) {
-			if ($referer == $current) {
-				return null;
-			}
+		if (!in_array($_SERVER['HTTP_REFERER'], $valid)) {
+			die(elgg_echo('src:error', array('info@elgg.org')));
 		}
-
-		//header('HTTP/1.0 301 Moved Permanently');
-		//header('Location: http://127.0.0.1');
-		//exit;
-		die('Invalid referrer for this action. Please contact info@elgg.org for more information.');
 	}
 
 	return null;
 }
 
+/**
+ * Log this action
+ */
+function src_log() {
+
+	$log = elgg_get_data_path() . 'src_log.txt';
+
+	$time = date('r');
+	ob_start();
+	print_r($_REQUEST);
+	$output = ob_get_contents();
+	ob_end_clean();
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$request = $_SERVER['REQUEST_URI'];
+	$referer = $_SERVER['HTTP_REFERER'];
+	$entry = "$time - $ip - $request - $referer\n$output";
+
+	$fp = fopen($log, 'a+b');
+	fputs($fp, $entry);
+	fclose($fp);
+}
+
+/**
+ * Unused tarpit for actions
+ */
 function src_tarpit() {
 	$h = fopen('/dev/random', 'rb');
 
@@ -50,26 +79,3 @@ function src_tarpit() {
 		$wait += $sleep;
 	}
 }
-
-function src_log() {
-        global $CONFIG;
-
-        $log = $CONFIG->dataroot . 'src_log.txt';
-
-        $time = date('r');
-	ob_start();
-	print_r($_REQUEST);
-	$output = ob_get_contents();
-	ob_end_clean();
-	$ip = $_SERVER['REMOTE_ADDR'];
-	$request = $_SERVER['REQUEST_URI'];
-	$referer = $_SERVER['HTTP_REFERER'];
-	$entry = "$time - $ip - $request - $referer\n$output";
-
-        $fp = fopen($log, 'a+b');
-        fputs($fp, $entry);
-        fclose($fp);
-}
-
-
-register_plugin_hook('action', 'all', 'src_check');
